9d78268632afefeebafb477329b749511c6eb319
KaKusaOAO <ka.kusaowo@gmail.com>
2023-12-17 10:27:52 +0800

    CMochi: Updated

--- src/components.cpp
+++ src/components.cpp
@@ -9,210 +9,210 @@
 
 namespace Mochi {
 
-// MARK: -
+    // MARK: -
 
-IContentVisitor::Ref IContentVisitor::Create(IContentVisitor::Signature action) {
-    class Instance : public IContentVisitor {
-    private:
-        IContentVisitor::Signature _delegate;
-    public:
-        Instance(IContentVisitor::Signature action) : _delegate(action) {
+    IContentVisitor::Ref IContentVisitor::Create(IContentVisitor::Signature action) {
+        class Instance : public IContentVisitor {
+        private:
+            IContentVisitor::Signature _delegate;
+        public:
+            Instance(IContentVisitor::Signature action) : _delegate(action) {
+                
+            }
             
-        }
+            void Accept(IContent::Ref content, IStyle::Ref style) override {
+                _delegate(content, style);
+            }
+        };
         
-        void Accept(IContent::Ref content, IStyle::Ref style) override {
-            _delegate(content, style);
-        }
-    };
-    
-    return Mochi::CreateRef<Instance>(action);
-}
-
-// MARK: -
-
-TextColor::Ref TextColor::RegisterBuiltin(char code, std::string name, Color color) {
-    TextColor::Ref result = Mochi::CreateRef<TextColor>(code, name, color);
-    _byChar.insert({code, result});
-    _byName.insert({name, result});
-    return result;
-}
-
-TextColor::TextColor(char code, std::string name, Color color) : _code(code), _name(name), _color(color) {
-    std::stringstream str;
-    str << ColorChar << code;
-    str >> _toString;
-    
-    _ordinal = _count++;
-}
-
-const std::string TextColor::ColorChar = "§";
-TextColor::ByCharMap TextColor::_byChar = TextColor::ByCharMap();
-TextColor::ByNameMap TextColor::_byName = TextColor::ByNameMap();
-int TextColor::_count = 0;
-
-#define __MC_DEFINE_COLOR(id, code, name, color) \
-const TextColor::Ref TextColor :: id = TextColor::RegisterBuiltin( code , #name, Color( color ));
-__MC_DEFINE_COLORS
-#undef __MC_DEFINE_COLOR
-
-// MARK: -
-
-TextColor::Ref BasicColoredStyle::GetColor() {
-    return _color;
-}
-
-IColoredStyle::Ref BasicColoredStyle::WithColor(TextColor::Ref color) {
-    _color = color;
-    return GetRef<IStyle>(this);
-}
-
-IStyle::Ref BasicColoredStyle::ApplyTo(IStyle::Ref other) {
-    auto o = Mochi::AssertSubType<IColoredStyle>(other);
-    
-    auto self = GetRef<IStyle>(this);
-    if (self == _empty) return o;
-    if (other == _empty) return self;
-    
-    auto color = _color;
-    if (color == nullptr) {
-        color = o->GetColor();
+        return Mochi::CreateRef<Instance>(action);
     }
-    
-    return CreateRef<BasicColoredStyle>()->WithColor(color);
-}
 
-void BasicColoredStyle::SerializeInto(Json::Value obj) {
-    
-}
+    // MARK: -
 
-IStyle::Ref BasicColoredStyle::Clear() {
-    return GetRef<IStyle>(this); // shared_from_this();
-}
-
-BasicColoredStyle::Ref BasicColoredStyle::_empty = CreateRef<BasicColoredStyle>();
-BasicColoredStyle::Ref BasicColoredStyle::Empty() { return _empty; }
-
-// MARK: -
-
-// MARK: TextContentTypes::_types
-TextContentTypes::Registry TextContentTypes::_types = TextContentTypes::Registry();
-
-// MARK: -
-
-LiteralContent::LiteralContent(std::string text)
-: text(text) {}
-
-IContentType::Ref LiteralContent::GetType() {
-    return TextContentTypes::Literal();
-}
-
-IContent::Ref LiteralContent::Clone() {
-    return CreateRef<LiteralContent>(text);
-}
-
-void LiteralContent::InsertPayload(Json::Value target) {
-    GetType()->InsertPayload(target, shared_from_this());
-}
-
-void LiteralContent::Visit(IContentVisitor::Ref visitor,
-                           IStyle::Ref style) {
-    visitor->Accept(GetRef<IContent>(this), style);
-}
-
-void LiteralContent::VisitLiteral(std::shared_ptr<IContentVisitor> visitor,
-    std::shared_ptr<IStyle> style) {
-    visitor->Accept(GetRef<IContent>(this), style);
-}
-
-// MARK: -
-
-std::shared_ptr<IContent> LiteralContentType::CreateContent(Json::Value payload) {
-    auto text = payload["text"].asString();
-    return CreateRef<LiteralContent>(text);
-}
-
-void LiteralContentType::InsertPayload(Json::Value target, IContent::Ref content) {
-    Mochi::ThrowNotImplemented();
-}
-
-std::shared_ptr<LiteralContentType> TextContentTypes::e_Literal = TextContentTypes::Register("text", std::make_shared<LiteralContentType>());
-std::shared_ptr<LiteralContentType> TextContentTypes::Literal() {
-    return e_Literal;
-}
-
-// MARK: -
-
-class GenericMutableComponent : public IMutableComponent {
-private:
-    IContent::Ref _content;
-    IStyle::Ref _style;
-    std::list<IComponent::Ref> _siblings;
-    
-public:
-    GenericMutableComponent(IContent::Ref content,
-                            IStyle::Ref style):
-    _content(content), _style(style), _siblings() {}
-    
-    IContent::Ref GetContent() override {
-        return _content;
-    }
-    
-    IStyle::Ref GetStyle() override {
-        return _style;
-    }
-    
-    void SetStyle(IStyle::Ref style) override {
-        _style = style;
-    }
-    
-    std::list<IComponent::Ref> GetSiblings() override {
-        return _siblings;
-    }
-    
-    IMutableComponent::Ref Clone() override {
-        auto result = CreateRef<GenericMutableComponent>(_content, _style);
-        for (auto sibling : _siblings) {
-            auto clone = sibling->Clone();
-            result->GetSiblings().push_back(clone);
-        }
-        
+    TextColor::Ref TextColor::RegisterBuiltin(char code, std::string name, Color color) {
+        TextColor::Ref result = Mochi::CreateRef<TextColor>(code, name, color);
+        _byChar.insert({code, result});
+        _byName.insert({name, result});
         return result;
     }
-    
-    void Visit(IContentVisitor::Ref visitor, IStyle::Ref style) override {
-        style = _style->ApplyTo(style);
-        _content->Visit(visitor, style);
+
+    TextColor::TextColor(char code, std::string name, Color color) : _code(code), _name(name), _color(color) {
+        std::stringstream str;
+        str << ColorChar << code;
+        str >> _toString;
         
-        for (auto sibling : _siblings) {
-            sibling->Visit(visitor, style);
-        }
+        _ordinal = _count++;
     }
-    
-    void VisitLiteral(IContentVisitor::Ref visitor, IStyle::Ref style) override {
-        style = _style->ApplyTo(style);
-        _content->VisitLiteral(visitor, style);
+
+    const std::string TextColor::ColorChar = "§";
+    TextColor::ByCharMap TextColor::_byChar = TextColor::ByCharMap();
+    TextColor::ByNameMap TextColor::_byName = TextColor::ByNameMap();
+    int TextColor::_count = 0;
+
+    #define __MC_DEFINE_COLOR(id, code, name, color) \
+    const TextColor::Ref TextColor :: id = TextColor::RegisterBuiltin( code , #name, Color( color ));
+    __MC_DEFINE_COLORS
+    #undef __MC_DEFINE_COLOR
+
+    // MARK: -
+
+    TextColor::Ref BasicColoredStyle::GetColor() {
+        return _color;
+    }
+
+    IColoredStyle::Ref BasicColoredStyle::WithColor(TextColor::Ref color) {
+        _color = color;
+        return GetRef<IStyle>(this);
+    }
+
+    IStyle::Ref BasicColoredStyle::ApplyTo(IStyle::Ref other) {
+        auto o = Mochi::AssertSubType<IColoredStyle>(other);
         
-        for (auto sibling : _siblings) {
-            sibling->VisitLiteral(visitor, style);
+        auto self = GetRef<IStyle>(this);
+        if (self == _empty) return o;
+        if (other == _empty) return self;
+        
+        auto color = _color;
+        if (color == nullptr) {
+            color = o->GetColor();
         }
+        
+        return CreateRef<BasicColoredStyle>()->WithColor(color);
     }
-};
 
-// MARK: -
+    void BasicColoredStyle::SerializeInto(Json::Value obj) {
+        
+    }
 
-IComponent::Ref Component::FromJson(Json::Value obj,
-                                    Component::JsonStyleParseFn parseStyle) {
-    Mochi::ThrowNotImplemented();
-}
+    IStyle::Ref BasicColoredStyle::Clear() {
+        return GetRef<IStyle>(this); // shared_from_this();
+    }
 
-IComponent::Ref Component::FromJson(Json::Value obj) {
-    Mochi::ThrowNotImplemented();
-    // return FromJson(obj, );
-}
+    BasicColoredStyle::Ref BasicColoredStyle::_empty = CreateRef<BasicColoredStyle>();
+    BasicColoredStyle::Ref BasicColoredStyle::Empty() { return _empty; }
 
-IComponent::Ref Component::Literal(std::string text) {
-    return std::make_shared<GenericMutableComponent>(std::make_shared<LiteralContent>(text),
-                                                     std::make_shared<BasicColoredStyle>());
-}
+    // MARK: -
+
+    // MARK: TextContentTypes::_types
+    TextContentTypes::Registry TextContentTypes::_types = TextContentTypes::Registry();
+
+    // MARK: -
+
+    LiteralContent::LiteralContent(std::string text)
+    : text(text) {}
+
+    IContentType::Ref LiteralContent::GetType() {
+        return TextContentTypes::Literal();
+    }
+
+    IContent::Ref LiteralContent::Clone() {
+        return CreateRef<LiteralContent>(text);
+    }
+
+    void LiteralContent::InsertPayload(Json::Value target) {
+        GetType()->InsertPayload(target, shared_from_this());
+    }
+
+    void LiteralContent::Visit(IContentVisitor::Ref visitor,
+                            IStyle::Ref style) {
+        visitor->Accept(GetRef<IContent>(this), style);
+    }
+
+    void LiteralContent::VisitLiteral(std::shared_ptr<IContentVisitor> visitor,
+        std::shared_ptr<IStyle> style) {
+        visitor->Accept(GetRef<IContent>(this), style);
+    }
+
+    // MARK: -
+
+    std::shared_ptr<IContent> LiteralContentType::CreateContent(Json::Value payload) {
+        auto text = payload["text"].asString();
+        return CreateRef<LiteralContent>(text);
+    }
+
+    void LiteralContentType::InsertPayload(Json::Value target, IContent::Ref content) {
+        Mochi::ThrowNotImplemented();
+    }
+
+    std::shared_ptr<LiteralContentType> TextContentTypes::e_Literal = TextContentTypes::Register("text", std::make_shared<LiteralContentType>());
+    std::shared_ptr<LiteralContentType> TextContentTypes::Literal() {
+        return e_Literal;
+    }
+
+    // MARK: -
+
+    class GenericMutableComponent : public IMutableComponent {
+    private:
+        IContent::Ref _content;
+        IStyle::Ref _style;
+        std::list<IComponent::Ref> _siblings;
+        
+    public:
+        GenericMutableComponent(IContent::Ref content,
+                                IStyle::Ref style):
+        _content(content), _style(style), _siblings() {}
+        
+        IContent::Ref GetContent() override {
+            return _content;
+        }
+        
+        IStyle::Ref GetStyle() override {
+            return _style;
+        }
+        
+        void SetStyle(IStyle::Ref style) override {
+            _style = style;
+        }
+        
+        std::list<IComponent::Ref> GetSiblings() override {
+            return _siblings;
+        }
+        
+        IMutableComponent::Ref Clone() override {
+            auto result = CreateRef<GenericMutableComponent>(_content, _style);
+            for (auto sibling : _siblings) {
+                auto clone = sibling->Clone();
+                result->GetSiblings().push_back(clone);
+            }
+            
+            return result;
+        }
+        
+        void Visit(IContentVisitor::Ref visitor, IStyle::Ref style) override {
+            style = _style->ApplyTo(style);
+            _content->Visit(visitor, style);
+            
+            for (auto sibling : _siblings) {
+                sibling->Visit(visitor, style);
+            }
+        }
+        
+        void VisitLiteral(IContentVisitor::Ref visitor, IStyle::Ref style) override {
+            style = _style->ApplyTo(style);
+            _content->VisitLiteral(visitor, style);
+            
+            for (auto sibling : _siblings) {
+                sibling->VisitLiteral(visitor, style);
+            }
+        }
+    };
+
+    // MARK: -
+
+    IComponent::Ref Component::FromJson(Json::Value obj,
+                                        Component::JsonStyleParseFn parseStyle) {
+        Mochi::ThrowNotImplemented();
+    }
+
+    IComponent::Ref Component::FromJson(Json::Value obj) {
+        Mochi::ThrowNotImplemented();
+        // return FromJson(obj, );
+    }
+
+    IComponent::Ref Component::Literal(std::string text) {
+        return std::make_shared<GenericMutableComponent>(std::make_shared<LiteralContent>(text),
+                                                        std::make_shared<BasicColoredStyle>());
+    }
 
 }
--- src/foundation.cpp
+++ src/foundation.cpp
@@ -12,139 +12,139 @@ namespace Mochi {
 
 void IDisposable::Dispose() {}
 
-// MARK: -
+    // MARK: -
 
-PreconditionFailedException::PreconditionFailedException(std::string message) : _message(message) {}
-const char* PreconditionFailedException::what() const noexcept {
-    return _message.c_str();
-}
-
-void Preconditions::InternalEnsure(Bool test, void* val, std::string message) {
-    if (test) return;
-    throw PreconditionFailedException(message);
-}
-
-// MARK: -
-
-Color::Color(UInt32 hex) {
-    R = (UInt8) ((hex >> 16) & 0xff);
-    G = (UInt8) ((hex >> 8) & 0xff);
-    B = (UInt8) (hex & 0xff);
-}
-
-Color::Color(double r, double g, double b) {
-    Preconditions::IsPositive(r, "r");
-    Preconditions::IsPositive(g, "g");
-    Preconditions::IsPositive(b, "b");
-    
-    R = (UInt8) (std::min(r, 1.0) * 255);
-    G = (UInt8) (std::min(g, 1.0) * 255);
-    B = (UInt8) (std::min(b, 1.0) * 255);
-}
-
-UInt32 Color::RGB() const {
-    return (R << 8 | G) << 8 | B;
-}
-
-void Color::Normalize(double *outR, double *outG, double *outB) {
-    if (outR) *outR = R / (double) 0xff;
-    if (outG) *outG = G / (double) 0xff;
-    if (outB) *outB = B / (double) 0xff;
-}
-
-void Color::ToHsv(double *outHue, double *outSaturation, double *outValue) {
-    if (!outHue && !outSaturation && !outValue) {
-        // We don't need to do calculations, so we don't run at all
-        return;
+    PreconditionFailedException::PreconditionFailedException(std::string message) : _message(message) {}
+    const char* PreconditionFailedException::what() const noexcept {
+        return _message.c_str();
     }
-    
-    double r, g, b;
-    Normalize(&r, &g, &b);
-    
-    double max = std::max({r, g, b});
-    double min = std::min({r, g, b});
-    double delta = max - min;
-    
-    double h, s, v;
-    const double d60 = Math::DegToRad * 60.0;
-    
-    if (delta == 0) {
-        h = 0;
-    } else if (std::abs(max - r) < std::numeric_limits<double>::epsilon() * 2) {
-        h = d60 * fmod((g - b) / delta, 6);
-    } else if (std::abs(max - g) < std::numeric_limits<double>::epsilon() * 2) {
-        h = d60 * ((b - r) / delta + 2);
-    } else if (std::abs(max - b) < std::numeric_limits<double>::epsilon() * 2) {
-        h = d60 * ((r - g) / delta + 4);
-    } else {
-        throw std::runtime_error("Shouldn't reach here");
-    }
-    
-    if (max == 0) {
-        s = 0;
-    } else {
-        s = delta / max;
-    }
-    
-    v = max;
-    
-    // Store the result
-    if (outHue)        *outHue = h;
-    if (outSaturation) *outSaturation = s;
-    if (outValue)      *outValue = v;
-}
 
-Color Color::FromHsv(double hue, double saturation, double value) {
-    Preconditions::IsPositive(saturation, "saturation");
-    Preconditions::IsPositive(value, "value");
-    
-    if (hue < 0) {
-        hue *= -1;
-        hue = fmod(hue, std::numbers::pi * 2);
-        hue *= -1;
-        hue += std::numbers::pi * 2;
-    } else {
-        hue = fmod(hue, std::numbers::pi * 2);
+    void Preconditions::InternalEnsure(Bool test, void* val, std::string message) {
+        if (test) return;
+        throw PreconditionFailedException(message);
     }
-    
-    saturation = std::min(1.0, saturation);
-    value = std::min(1.0, value);
-    
-    const double d60 = Math::DegToRad * 60.0;
-    const double d120 = d60 * 2;
-    const double d180 = d60 * 3;
-    const double d240 = d60 * 4;
-    const double d300 = d60 * 5;
-    
-    double c = value * saturation;
-    double x = c * (1.0 - std::abs(fmod(hue / d60, 2) - 1));
-    double m = value - c;
-    
-    double r = 0;
-    double g = 0;
-    double b = 0;
-    
-    if (0 <= hue && hue < d60) {
-        r = c;
-        g = x;
-    } else if (d60 <= hue && hue < d120) {
-        r = x;
-        g = c;
-    } else if (d120 <= hue && hue < d180) {
-        g = c;
-        b = x;
-    } else if (d180 <= hue && hue < d240) {
-        g = x;
-        b = c;
-    } else if (d240 <= hue && hue < d300) {
-        b = c;
-        r = x;
-    } else {
-        b = x;
-        r = c;
+
+    // MARK: -
+
+    Color::Color(UInt32 hex) {
+        R = (UInt8) ((hex >> 16) & 0xff);
+        G = (UInt8) ((hex >> 8) & 0xff);
+        B = (UInt8) (hex & 0xff);
     }
-    
-    return {r + m, g + m, b + m};
-}
+
+    Color::Color(double r, double g, double b) {
+        Preconditions::IsPositive(r, "r");
+        Preconditions::IsPositive(g, "g");
+        Preconditions::IsPositive(b, "b");
+        
+        R = (UInt8) (std::min(r, 1.0) * 255);
+        G = (UInt8) (std::min(g, 1.0) * 255);
+        B = (UInt8) (std::min(b, 1.0) * 255);
+    }
+
+    UInt32 Color::RGB() const {
+        return (R << 8 | G) << 8 | B;
+    }
+
+    void Color::Normalize(double *outR, double *outG, double *outB) {
+        if (outR) *outR = R / (double) 0xff;
+        if (outG) *outG = G / (double) 0xff;
+        if (outB) *outB = B / (double) 0xff;
+    }
+
+    void Color::ToHsv(double *outHue, double *outSaturation, double *outValue) {
+        if (!outHue && !outSaturation && !outValue) {
+            // We don't need to do calculations, so we don't run at all
+            return;
+        }
+        
+        double r, g, b;
+        Normalize(&r, &g, &b);
+        
+        double max = std::max({r, g, b});
+        double min = std::min({r, g, b});
+        double delta = max - min;
+        
+        double h, s, v;
+        const double d60 = Math::DegToRad * 60.0;
+        
+        if (delta == 0) {
+            h = 0;
+        } else if (std::abs(max - r) < std::numeric_limits<double>::epsilon() * 2) {
+            h = d60 * fmod((g - b) / delta, 6);
+        } else if (std::abs(max - g) < std::numeric_limits<double>::epsilon() * 2) {
+            h = d60 * ((b - r) / delta + 2);
+        } else if (std::abs(max - b) < std::numeric_limits<double>::epsilon() * 2) {
+            h = d60 * ((r - g) / delta + 4);
+        } else {
+            throw std::runtime_error("Shouldn't reach here");
+        }
+        
+        if (max == 0) {
+            s = 0;
+        } else {
+            s = delta / max;
+        }
+        
+        v = max;
+        
+        // Store the result
+        if (outHue)        *outHue = h;
+        if (outSaturation) *outSaturation = s;
+        if (outValue)      *outValue = v;
+    }
+
+    Color Color::FromHsv(double hue, double saturation, double value) {
+        Preconditions::IsPositive(saturation, "saturation");
+        Preconditions::IsPositive(value, "value");
+        
+        if (hue < 0) {
+            hue *= -1;
+            hue = fmod(hue, std::numbers::pi * 2);
+            hue *= -1;
+            hue += std::numbers::pi * 2;
+        } else {
+            hue = fmod(hue, std::numbers::pi * 2);
+        }
+        
+        saturation = std::min(1.0, saturation);
+        value = std::min(1.0, value);
+        
+        const double d60 = Math::DegToRad * 60.0;
+        const double d120 = d60 * 2;
+        const double d180 = d60 * 3;
+        const double d240 = d60 * 4;
+        const double d300 = d60 * 5;
+        
+        double c = value * saturation;
+        double x = c * (1.0 - std::abs(fmod(hue / d60, 2) - 1));
+        double m = value - c;
+        
+        double r = 0;
+        double g = 0;
+        double b = 0;
+        
+        if (0 <= hue && hue < d60) {
+            r = c;
+            g = x;
+        } else if (d60 <= hue && hue < d120) {
+            r = x;
+            g = c;
+        } else if (d120 <= hue && hue < d180) {
+            g = c;
+            b = x;
+        } else if (d180 <= hue && hue < d240) {
+            g = x;
+            b = c;
+        } else if (d240 <= hue && hue < d300) {
+            b = c;
+            r = x;
+        } else {
+            b = x;
+            r = c;
+        }
+        
+        return {r + m, g + m, b + m};
+    }
 
 }
